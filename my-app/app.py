e212c95e6d67831c3b415c7436ca40c93c0d9957
jhochedez <148982106+jhochedez@users.noreply.github.com>
2023-10-26 11:40:26 +0200

    Update app.py

--- my-app/app.py
+++ my-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, {}!</p>".format(tower_user_name)
 
 @app.route("/tp")
 def hello_2():
